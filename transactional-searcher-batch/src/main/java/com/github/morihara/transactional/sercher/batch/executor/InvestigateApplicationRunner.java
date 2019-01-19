cf0da00bb2ccbb4fca2c1036f3c3728d01baddc3
Morihara Yutaka <yutaka.morihara@gmail.com>
2019-01-19 22:31:18 +0800

    make service interface

--- transactional-searcher-batch/src/main/java/com/github/morihara/transactional/sercher/batch/executor/InvestigateApplicationRunner.java
+++ transactional-searcher-batch/src/main/java/com/github/morihara/transactional/sercher/batch/executor/InvestigateApplicationRunner.java
@@ -1,19 +1,35 @@
 package com.github.morihara.transactional.sercher.batch.executor;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import com.github.morihara.transactional.sercher.biz.service.InvestigationService;
+import com.github.morihara.transactional.sercher.dto.TransactionalMethodDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Slf4j
 public class InvestigateApplicationRunner implements CommandLineRunner {
+    private final InvestigationService investigationService;
 
     @Override
     public void run(String... arg0) throws Exception {
-        log.info("----start----");
- 
-        log.info("----end----");
+        log.info("----start investigation----");
+        String sourceFolderPath = arg0[0];
+        List<TransactionalMethodDto> transactionalMethodDtos =
+                investigationService.getTopLayerWithoutTransactional(sourceFolderPath);
+        for (TransactionalMethodDto transactionalMethodDto : transactionalMethodDtos) {
+            if (!investigationService.isRDBUpdateService(transactionalMethodDto)) {
+                continue;
+            }
+            if (investigationService.isManagedTransactional(transactionalMethodDto)) {
+                continue;
+            }
+            investigationService.updateResult(transactionalMethodDto);
+        }
+        investigationService.exportCSV(sourceFolderPath);
+        log.info("----end investigation----");
     }
 
 }
--- transactional-searcher-biz/src/main/java/com/github/morihara/transactional/sercher/biz/service/InvestigationService.java
+++ transactional-searcher-biz/src/main/java/com/github/morihara/transactional/sercher/biz/service/InvestigationService.java
@@ -1,5 +1,16 @@
 package com.github.morihara.transactional.sercher.biz.service;
 
-public interface InvestigationService {
+import java.util.List;
+import com.github.morihara.transactional.sercher.dto.TransactionalMethodDto;
 
+public interface InvestigationService {
+    List<TransactionalMethodDto> getTopLayerWithoutTransactional(String sourceFolderPath);
+
+    boolean isRDBUpdateService(TransactionalMethodDto transactionalMethodDto);
+
+    boolean isManagedTransactional(TransactionalMethodDto transactionalMethodDto);
+
+    void updateResult(TransactionalMethodDto transactionalMethodDto);
+
+    void exportCSV(String sourceFolderPath);
 }
