aafc1b0fe2d82d5eb0b4621df2b4e92c24783f3c
Morihara Yutaka <yutaka.morihara@gmail.com>
2019-01-20 23:24:42 +0800

    refactoring for easy reading

--- transactional-searcher-biz/src/main/java/com/github/morihara/transactional/sercher/biz/source/search/service/CallHierarchyServiceImpl.java
+++ transactional-searcher-biz/src/main/java/com/github/morihara/transactional/sercher/biz/source/search/service/CallHierarchyServiceImpl.java
@@ -2,9 +2,7 @@ package com.github.morihara.transactional.sercher.biz.source.search.service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 import org.springframework.stereotype.Service;
-import com.github.morihara.transactional.sercher.dao.spoon.HierarchyResearchDao;
 import com.github.morihara.transactional.sercher.dto.RelatedDaoCodeDto;
 import com.github.morihara.transactional.sercher.dto.TransactionalMethodDto;
 import com.github.morihara.transactional.sercher.dto.vo.HierarchyVo;
@@ -14,25 +12,29 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class CallHierarchyServiceImpl implements CallHierarchyService {
-    private final HierarchyResearchDao hierarchyResearchDao;
 
     @Override
     public List<RelatedDaoCodeDto> fetchRelatedDaoCodesByCallHierarchy(
             TransactionalMethodDto transactionalMethodDto) {
-        UUID transactionalMethodId = transactionalMethodDto.getTransactionalMethodId();
+        List<HierarchyVo> calledHierarchyList = callHierarchy(transactionalMethodDto.getSourceCodeVo());
+        return makeResultList(transactionalMethodDto, calledHierarchyList);
+    }
+
+    private List<HierarchyVo> callHierarchy(SourceCodeVo sourceCodeVo) {
+        return null;
+    }
+
+    private List<RelatedDaoCodeDto> makeResultList(TransactionalMethodDto transactionalMethodDto,
+            List<HierarchyVo> calledHierarchyList) {
         List<RelatedDaoCodeDto> resultDaoCodes = new ArrayList<>();
-
-        List<HierarchyVo> hierarchyVoList =
-                hierarchyResearchDao.callHierarchy(transactionalMethodDto.getSourceCodeVo());
-
         int seq = 0;
-        for (HierarchyVo hierarchyVo : hierarchyVoList) {
+        for (HierarchyVo hierarchyVo : calledHierarchyList) {
             if (hierarchyVo.isDao() && hierarchyVo.isRequiredTransactional()) {
                 SourceCodeVo fetchedMethodVo = hierarchyVo.getSourceCodeVo();
                 seq++;
                 resultDaoCodes.add(
                         RelatedDaoCodeDto.builder()
-                            .transactionalMethodId(transactionalMethodId)
+                            .transactionalMethodId(transactionalMethodDto.getTransactionalMethodId())
                             .seq(seq)
                             .relatedDaoCodeVo(fetchedMethodVo)
                             .build());
--- transactional-searcher-dao/src/main/java/com/github/morihara/transactional/sercher/dao/spoon/HierarchyResearchDao.java
+++
@@ -1,9 +0,0 @@
-package com.github.morihara.transactional.sercher.dao.spoon;
-
-import java.util.List;
-import com.github.morihara.transactional.sercher.dto.vo.HierarchyVo;
-import com.github.morihara.transactional.sercher.dto.vo.SourceCodeVo;
-
-public interface HierarchyResearchDao {
-    List<HierarchyVo> callHierarchy(SourceCodeVo sourceCodeVo);
-}
