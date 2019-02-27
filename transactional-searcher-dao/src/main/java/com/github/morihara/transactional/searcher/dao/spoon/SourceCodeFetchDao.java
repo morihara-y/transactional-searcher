81119e99684deb33ec3a4c54fc1d8476c1d375d3
Morihara Yutaka <yutaka.morihara@gmail.com>
2019-02-27 21:53:35 +0800

    remake interface for multiple jar

--- transactional-searcher-dao/src/main/java/com/github/morihara/transactional/searcher/dao/spoon/SourceCodeFetchDao.java
+++ transactional-searcher-dao/src/main/java/com/github/morihara/transactional/searcher/dao/spoon/SourceCodeFetchDao.java
@@ -1,24 +1,27 @@
 package com.github.morihara.transactional.searcher.dao.spoon;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Map;
-
 import com.github.morihara.transactional.searcher.dto.vo.BeanDefinitionVo;
+import com.github.morihara.transactional.searcher.dto.vo.MetadataResourceVo;
 import com.github.morihara.transactional.searcher.dto.vo.SourceCodeVo;
 
 public interface SourceCodeFetchDao {
-    List<String> fetchPackagesBySourceFolderPath(String sourceFolderPath);
-
-    List<SourceCodeVo> fetchMethodsByPackageName(String sourceFolderPath, String packageName);
-
-    List<SourceCodeVo> fetchCalledMethodsByMethod(String sourceFolderPath, SourceCodeVo sourceCodeVo,
-            List<String> packagePrefixList);
-
-    int hasMethod(String sourceFolderPath, SourceCodeVo sourceCodeVo, Method[] methods);
-
-    boolean hasAnnotation(String sourceFolderPath, SourceCodeVo sourceCodeVo, Class<?> annotationType);
-
-    void updateBeanDefinitionMap(String sourceFolderPath, String springConfigPath, List<String> packagePrefixList,
+    void walkJarFile(String jarPath, Map<String, MetadataResourceVo> metadataResourceMap,
             Map<String, List<BeanDefinitionVo>> beanDefinitionMap);
+
+    List<SourceCodeVo> fetchPublicMethodsByAnotation(Annotation annotation,
+            Map<String, MetadataResourceVo> metadataResourceMap);
+
+    List<SourceCodeVo> fetchCalledMethodsByMethod(SourceCodeVo sourceCodeVo,
+            List<String> daoPackageNames, Map<String, MetadataResourceVo> metadataResourceMap,
+            Map<String, List<BeanDefinitionVo>> beanDefinitionMap);
+
+    int hasMethod(SourceCodeVo sourceCodeVo, Method[] methods,
+            Map<String, MetadataResourceVo> metadataResourceMap);
+
+    boolean hasAnnotation(SourceCodeVo sourceCodeVo, Class<?> annotationType,
+            Map<String, MetadataResourceVo> metadataResourceMap);
 }
--- transactional-searcher-dao/src/main/java/com/github/morihara/transactional/searcher/dao/spoon/SourceCodeFetchDaoImpl.java
+++ transactional-searcher-dao/src/main/java/com/github/morihara/transactional/searcher/dao/spoon/SourceCodeFetchDaoImpl.java
@@ -2,6 +2,7 @@ package com.github.morihara.transactional.searcher.dao.spoon;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Map;
@@ -11,9 +12,11 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Repository;
 
 import com.github.morihara.transactional.searcher.dto.vo.BeanDefinitionVo;
+import com.github.morihara.transactional.searcher.dto.vo.MetadataResourceVo;
 import com.github.morihara.transactional.searcher.dto.vo.SourceCodeVo;
 
 import lombok.extern.slf4j.Slf4j;
+import spoon.JarLauncher;
 import spoon.Launcher;
 import spoon.SpoonException;
 import spoon.support.QueueProcessingManager;
@@ -25,6 +28,43 @@ import spoon.support.compiler.FileSystemFolder;
 public class SourceCodeFetchDaoImpl implements SourceCodeFetchDao {
 
     @Override
+    public void walkJarFile(String jarPath, Map<String, MetadataResourceVo> metadataResourceMap,
+            Map<String, List<BeanDefinitionVo>> beanDefinitionMap) {
+        JarLauncher launcher = new JarLauncher(jarPath);
+        launcher.setArgs(new String[] {"--output-type", "nooutput"});
+        launcher.run();
+        QueueProcessingManager queueProcessingManager = new QueueProcessingManager(launcher.getFactory());
+    }
+
+    @Override
+    public List<SourceCodeVo> fetchPublicMethodsByAnotation(Annotation annotation,
+            Map<String, MetadataResourceVo> metadataResourceMap) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<SourceCodeVo> fetchCalledMethodsByMethod(SourceCodeVo sourceCodeVo,
+            List<String> daoPackageNames, Map<String, MetadataResourceVo> metadataResourceMap,
+            Map<String, List<BeanDefinitionVo>> beanDefinitionMap) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int hasMethod(SourceCodeVo sourceCodeVo, Method[] methods,
+            Map<String, MetadataResourceVo> metadataResourceMap) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean hasAnnotation(SourceCodeVo sourceCodeVo, Class<?> annotationType,
+            Map<String, MetadataResourceVo> metadataResourceMap) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
     public List<String> fetchPackagesBySourceFolderPath(String sourceFolderPath) {
         Launcher launcher = makeLauncherWithCommonArgs();
         launcher.addInputResource(new FileSystemFolder(new File(sourceFolderPath)));
@@ -33,7 +73,6 @@ public class SourceCodeFetchDaoImpl implements SourceCodeFetchDao {
         return new FetchPackagesProcesser().executeSpoon(queueProcessingManager);
     }
 
-    @Override
     public List<SourceCodeVo> fetchMethodsByPackageName(String sourceFolderPath, String packageName) {
         String packageNamePath = makePackagePath(sourceFolderPath, packageName);
         Launcher launcher = makeLauncherWithCommonArgs();
@@ -43,7 +82,6 @@ public class SourceCodeFetchDaoImpl implements SourceCodeFetchDao {
         return new FetchMethodsProcesser(packageName).executeSpoon(queueProcessingManager);
     }
 
-    @Override
     public List<SourceCodeVo> fetchCalledMethodsByMethod(String sourceFolderPath,
             SourceCodeVo sourceCodeVo, List<String> packagePrefixList) {
         String classPath = makeClassPath(sourceFolderPath, sourceCodeVo);
@@ -54,7 +92,6 @@ public class SourceCodeFetchDaoImpl implements SourceCodeFetchDao {
         return new FetchChildMethodsProcesser(sourceCodeVo, packagePrefixList).executeSpoon(queueProcessingManager);
     }
 
-    @Override
     public int hasMethod(String sourceFolderPath, SourceCodeVo sourceCodeVo, Method[] methods) {
         String classPath = makeClassPath(sourceFolderPath, sourceCodeVo);
         Launcher launcher = makeLauncherWithCommonArgs();
@@ -64,7 +101,6 @@ public class SourceCodeFetchDaoImpl implements SourceCodeFetchDao {
         return new CountTargetMethodsProcesser(sourceCodeVo, methods).executeSpoon(queueProcessingManager);
     }
 
-    @Override
     public boolean hasAnnotation(String sourceFolderPath, SourceCodeVo sourceCodeVo, Class<?> annotationType) {
         String classPath = makeClassPath(sourceFolderPath, sourceCodeVo);
         Launcher launcher = makeLauncherWithCommonArgs();
@@ -74,7 +110,6 @@ public class SourceCodeFetchDaoImpl implements SourceCodeFetchDao {
         return new ConfirmTargetAnnotationProcesser(sourceCodeVo, annotationType).executeSpoon(queueProcessingManager);
     }
 
-    @Override
     public void updateBeanDefinitionMap(String sourceFolderPath, String springConfigPath,
             List<String> packagePrefixList, Map<String, List<BeanDefinitionVo>> beanDefinitionMap) {
         String classPath = makeClassPath(sourceFolderPath, springConfigPath);
