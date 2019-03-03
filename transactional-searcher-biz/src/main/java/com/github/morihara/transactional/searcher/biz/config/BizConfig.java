ecf7fa0462027cf24fe449c3fb3e7a2fc1aba6d3
Morihara Yutaka <yutaka.morihara@gmail.com>
2019-03-03 22:06:50 +0800

    change config

--- transactional-searcher-biz/src/main/java/com/github/morihara/transactional/searcher/biz/config/BizConfig.java
+++ transactional-searcher-biz/src/main/java/com/github/morihara/transactional/searcher/biz/config/BizConfig.java
@@ -3,8 +3,7 @@ package com.github.morihara.transactional.searcher.biz.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-
-import com.github.morihara.transactional.searcher.dao.config.TestDaoConfig1;
+import com.github.morihara.transactional.searcher.dao.test.config.TestDaoConfig1;
 
 @Configuration
 @ComponentScan({"com.github.morihara.transactional.searcher.biz"})
