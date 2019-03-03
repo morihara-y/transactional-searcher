45194348cb8d18f4b278b042179857394bf1fa28
Morihara Yutaka <yutaka.morihara@gmail.com>
2019-03-03 19:43:34 +0800

    move test configs

--- transactional-searcher-dao/src/main/java/com/github/morihara/transactional/searcher/dao/test/config/TestDaoConfig3.java
+++ transactional-searcher-dao/src/main/java/com/github/morihara/transactional/searcher/dao/test/config/TestDaoConfig3.java
@@ -1,4 +1,4 @@
-package com.github.morihara.transactional.searcher.dao.config;
+package com.github.morihara.transactional.searcher.dao.test.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
